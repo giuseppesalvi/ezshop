f483510fe35d47f79ec1d468d42e67e01b78c648
Milad Beigi <milad.beigi@carnext.com>
2021-05-16 22:28:20 +0200

    increase coverage for UserImpTests

--- src/test/java/it/polito/ezshop/test/UserImplTests.java
+++ src/test/java/it/polito/ezshop/test/UserImplTests.java
@@ -1,6 +1,7 @@
 package it.polito.ezshop.test;
 
 import it.polito.ezshop.model.UserImpl;
+import it.polito.ezshop.data.EZShopMaps;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,35 +14,60 @@ public class UserImplTests {
         assertNotNull(user0);
         assertNotNull(user1);
     }
-
     @Test
     public void testUserImplWithNullID() {
         UserImpl user = new UserImpl("user","password","Admin");
         assertNotNull(user.getId());
     }
-
     @Test
     public void testGetId() {
         UserImpl user = new UserImpl("user","password","Admin", 123);
         assertEquals(java.util.Optional.ofNullable(user.getId()),java.util.Optional.ofNullable(123));
     }
-
+    @Test
+    public void testSetIdWithValidId () {
+        EZShopMaps.loadMaps();
+        UserImpl user = new UserImpl("user","password","Admin", 123);
+        user.setId(456);
+        assertEquals(java.util.Optional.ofNullable(user.getId()),java.util.Optional.of(456));
+    }
     @Test
     public void testGetUsername() {
         UserImpl user = new UserImpl("user","password","Admin");
         assertEquals(user.getUsername(),"user");
     }
-
+    @Test
+    public void testSetUsername() {
+        // TODO test with invalid inputs
+        EZShopMaps.loadMaps();
+        UserImpl user = new UserImpl("user","password","Admin", 123);
+        user.setUsername("username");
+        assertEquals(user.getUsername(),"username");
+    }
     @Test
     public void testGetPassword() {
         UserImpl user = new UserImpl("user","password","Admin");
         assertEquals(user.getPassword(),"password");
     }
-
+    @Test
+    public void testSetPassword() {
+        // TODO test with invalid inputs
+        EZShopMaps.loadMaps();
+        UserImpl user = new UserImpl("user","password","Admin", 123);
+        user.setPassword("pass");
+        assertEquals(user.getPassword(),"pass");
+    }
     @Test
     public void testGetRole() {
         UserImpl user = new UserImpl("user","password","Admin");
         assertEquals(user.getRole(),"Admin");
     }
-
+    @Test
+    public void testSetRole(){
+        // TODO test with invalid inputs
+        EZShopMaps.loadMaps();
+        UserImpl user = new UserImpl("user","password","Admin", 123);
+        user.setRole("Administrator");
+        assertEquals(user.getRole(),"Administrator");
+    }
 }
