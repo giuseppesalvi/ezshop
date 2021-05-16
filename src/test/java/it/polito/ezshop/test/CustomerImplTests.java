a1ab73d59f0d7dbb9a285cf7f84d61463bcd03c7
Milad Beigi <milad.beigi@carnext.com>
2021-05-16 23:06:03 +0200

    increase coverage for CustomerImpTest

--- src/test/java/it/polito/ezshop/test/CustomerImplTests.java
+++ src/test/java/it/polito/ezshop/test/CustomerImplTests.java
@@ -1,5 +1,7 @@
 package it.polito.ezshop.test;
+
 import it.polito.ezshop.model.CustomerImpl;
+import it.polito.ezshop.model.LoyaltyCard;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -32,14 +34,28 @@ public class CustomerImplTests {
     }
     @Test
     public void testGetPoints() {
+        LoyaltyCard card = new LoyaltyCard();
+        CustomerImpl customer = new CustomerImpl("name",123,card);
+        assertEquals(java.util.Optional.of(customer.getPoints()),java.util.Optional.of(0));
     }
     @Test
     public void testSetPoints() {
+        LoyaltyCard card = new LoyaltyCard();
+        CustomerImpl customer = new CustomerImpl("name",123,card);
+        customer.setPoints(100);
+        assertEquals(java.util.Optional.of(customer.getPoints()),java.util.Optional.of(100));
     }
     @Test
     public void testGetCard() {
+        LoyaltyCard card = new LoyaltyCard("123");
+        CustomerImpl customer = new CustomerImpl("name",123,card);
+        assertEquals(customer.getCard(),card);
     }
     @Test
     public void testSetCard() {
+        LoyaltyCard card = new LoyaltyCard("123");
+        CustomerImpl customer = new CustomerImpl("name");
+        customer.setCard(card);
+        assertEquals(customer.getCard(),card);
     }
 }
